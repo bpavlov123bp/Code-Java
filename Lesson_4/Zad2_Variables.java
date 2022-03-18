b4b41a1a69e2fd950a2a0c6c181d39854d950ed1
bpavlov123bp <bpavlov123bp@gmail.com>
2022-03-18 11:09:20 +0200

    Add files via upload

---
+++ Lesson_4/Zad2_Variables.java
@@ -0,0 +1,13 @@
+public class Zad2_Variables {
+    public static void main(String[] args)
+    {
+        float num1 = 4.9876543f;
+        System.out.println(num1);
+        double num2 = 7.123_456_789_012_345_678_890;
+        System.out.println(num2);
+        float num3 = 18_398_458_438_438_583_853.28f;
+        System.out.println(num3);
+        double num4 = 18_398_458_438_583_853.398_759_372_849_284_422;
+        System.out.println(num4);
+    }
+}
