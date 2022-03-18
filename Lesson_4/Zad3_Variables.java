0d190225518e619906f25b8b06858c8a29f4c0c5
bpavlov123bp <bpavlov123bp@gmail.com>
2022-03-18 11:00:33 +0200

    Add files via upload

---
+++ Lesson_4/Zad3_Variables.java
@@ -0,0 +1,15 @@
+public class Zad3_Variables {
+    public static void main(String[] args)
+    {
+        int num1 = 43112;
+        System.out.println(num1);
+        int num2 = -1_357_674;
+        System.out.println(num2);
+        int num3 = 1_357_674;
+        System.out.println(num3);
+        long num4 = -1_357_674_000L;
+        System.out.println(num4);
+        long num5 = 3_657_895_000L;
+        System.out.println(num5);
+    }
+}
