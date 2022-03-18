7d0700ddf929e89b0bf08597749fe9691dd3eb57
bpavlov123bp <bpavlov123bp@gmail.com>
2022-03-18 10:50:03 +0200

    Add files via upload

---
+++ Lesson_4/Zad1_Variables.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+public class Zad1_Variables {
+    public static void main(String[] args)
+    {
+        short num1 = -250;
+        System.out.println(num1);
+        short num2 = 250;
+        System.out.println(num2);
+        int num3 = 4_589_498;
+        System.out.println(num3);
+        BigInteger num4 = new BigInteger("10000000000000000000");
+        System.out.println(num4);
+        long num5 = -9_000_000_000_000_000_000L;
+        System.out.println(num5);
+    }
+}
