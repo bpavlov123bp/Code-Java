fba0d718482ab62e836080800640ffe28c0bb64c
bpavlov123bp <bpavlov123bp@gmail.com>
2022-03-20 16:27:59 +0200

    Add files via upload

---
+++ Lesson_6/Zad3_Lesson6.java
@@ -0,0 +1,13 @@
+public class Zad3_Lesson6 {
+    public static void main(String[] args) {
+        System.out.println("--------------------");
+        System.out.printf("|%-11s |%5s|\n", "Name", "Grade");
+        System.out.println("--------------------");
+        System.out.printf("|%-11s |% 5d|\n", "Ivancho", 2);
+        System.out.printf("|%-11s |% 5d|\n", "Any", 5);
+        System.out.printf("|%-11s |% 5d|\n", "Marika", 6);
+        System.out.printf("|%-11s |% 5d|\n", "Pencho", 4);
+        System.out.printf("|%-11s |% 5d|\n", "Borislavcho", 5);
+        System.out.println("--------------------");
+    }
+}
