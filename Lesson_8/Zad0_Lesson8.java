b41c80a58361ac87895bd7c2259e56f1f7bca9fc
bpavlov123bp <bpavlov123bp@gmail.com>
2022-03-26 19:41:27 +0200

    Add files via upload

---
+++ Lesson_8/Zad0_Lesson8.java
@@ -0,0 +1,15 @@
+public class Zad0_Lesson8 {
+    public static void main(String[] args)
+    {
+        int balls = 20;
+        balls -= 2;
+        balls -= 5;
+        balls -= 3;
+        balls += 2;
+        balls -= 4;
+        balls -= 6;
+        balls += 5;
+        balls -= 2;
+        System.out.println(balls);
+    }
+}
