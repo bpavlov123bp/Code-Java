328cade283ec03794db3f92f6ed8fa4adfdb9035
bpavlov123bp <bpavlov123bp@gmail.com>
2022-04-07 21:54:14 +0300

    Add files via upload

---
+++ Lesson_10/Zad3_Lesson10.java
@@ -0,0 +1,14 @@
+
+public class Zad3_Lesson10 {
+    public static void main(String[] args)
+    {
+        for(int row = 5; row >= 1; row--)
+        {
+            for(int col = 1; col <= row; col++)
+            {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+}
