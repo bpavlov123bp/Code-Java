879ee748c1cc022ecd7e5b9697d90015cbcf80f6
bpavlov123bp <bpavlov123bp@gmail.com>
2022-04-07 20:16:18 +0300

    Add files via upload

---
+++ Lesson_10/Number1To9.java
@@ -0,0 +1,11 @@
+public class Number1To9 {
+    public static void main(String[] args)
+    {
+        int counter = 0;
+        while(counter < 10)
+        {
+            System.out.printf("Number: %d\n", counter);
+            counter++;
+        }
+    }
+}
