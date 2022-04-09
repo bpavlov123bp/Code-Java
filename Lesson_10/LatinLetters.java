5ba47c265047f2a91d8c88b34ea27284074d4a53
bpavlov123bp <bpavlov123bp@gmail.com>
2022-04-09 21:12:41 +0300

    Add files via upload

---
+++ Lesson_10/LatinLetters.java
@@ -0,0 +1,11 @@
+public class LatinLetters {
+    public static void main(String[] args)
+    {
+        System.out.print("Latin alphabet: ");
+        for(char letter = 'a'; letter <= 'z'; letter++)
+        {
+            System.out.print(" " + letter);
+        }
+        System.out.println();
+    }
+}
