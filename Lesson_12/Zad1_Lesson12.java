4a85389388164b636377f74e6ab0c67480ac388c
bpavlov123bp <bpavlov123bp@gmail.com>
2022-04-14 21:24:11 +0300

    Add files via upload

---
+++ Lesson_12/Zad1_Lesson12.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Zad1_Lesson12 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dying swans twisted wings, beauty not needed here");
+        System.out.println("Lost my love, lost my life, in this garden of fear");
+        System.out.println("I have seen many things, in a lifetime alone");
+        System.out.println("Mother love is no more, bring this savage back home");
+        System.out.println("All is lost sold your souls to this brave new world");
+        textIronMaiden();
+        textIronMaiden();
+        textIronMaiden();
+        textIronMaiden();
+        System.out.println("Dragon kings dying queens, where is salvation now");
+        System.out.println("Lost my life lost my dreams, rip the bones from my flesh");
+        System.out.println("You are planned and you are damned in this brave new world");
+        textIronMaiden();
+        textIronMaiden();
+        textIronMaiden();
+    }
+    public static void textIronMaiden()
+    {
+        String text = "A brave new world, in a brave new world";
+        System.out.println(text);
+    }
+}
