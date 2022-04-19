45d0a8736c6852d2b253928f5e4b1deae145e402
bpavlov123bp <bpavlov123bp@gmail.com>
2022-04-19 20:00:32 +0300

    Add files via upload

--- Lesson_13/Zad3_Lesson13.java
+++ Lesson_13/Zad3_Lesson13.java
@@ -1,20 +1,25 @@
 import java.util.*;
 public class Zad3_Lesson13 {
-
     public static void main(String[] args)
+    {
+        dice();
+    }
+    public static void dice()
     {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
+        System.out.print("Enter number of players: ");
         int players = input.nextInt();
         int[] dice = new int[players];
-        //int[] total = new int[players];
         int sum = 0;
         int max = Integer.MIN_VALUE;
         int count = 0;
+
         for(int i = 0; i < players; i++)
         {
-           dice[i] = rand.nextInt(6);
+                System.out.println("Player " + i + ": ");
 
+            dice[i] = rand.nextInt(6) + 1;
         }
         for(int q = 0; q < dice.length; q++)
         {
@@ -28,7 +33,6 @@ public class Zad3_Lesson13 {
             }
             System.out.println(dice[q]);
         }
-        System.out.println(max);
-        System.out.println(count);
+        System.out.println("Player " + (count + 1) + " win with " + max);
     }
 }
