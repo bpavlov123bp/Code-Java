cc97474450e762632da931b23f84502f0de964ee
bpavlov123bp <bpavlov123bp@gmail.com>
2022-04-25 21:45:34 +0300

    Add files via upload

--- Lesson_14/TheMatrix.java
+++ Lesson_14/TheMatrix.java
@@ -4,38 +4,54 @@ public class TheMatrix {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter range of matrix: ");
-        int n = input.nextInt();
-        int[][] A = new int[n][n];
-        int[][] B = new int[n][n];
-        int[][] sumMatrix = new int[n][n];
-        System.out.println("Matrix A");
-        for(int i = 0; i < n; i++)
+        System.out.print("Enter matrix row: ");
+        int matrixRow = input.nextInt();
+        System.out.print("Enter matrix colmun: ");
+        int matrixCol = input.nextInt();
+        int[][] A = new int[matrixRow][matrixCol];
+        int[][] B = new int[matrixRow][matrixCol];
+        int[][] sumMatrix = new int[matrixRow][matrixCol];
+
+        for(int i = 0; i < matrixRow; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < matrixCol; j++)
             {
-                A[i][j] = (int)(20 * random() - 9);
+                A[i][j] = input.nextInt();
+
+            }
+        }
+        System.out.println("Matrix A");
+        for(int i = 0; i < matrixRow; i++)
+        {
+            for(int j = 0; j < matrixCol; j++)
+            {
                 System.out.printf("%4d", A[i][j]);
             }
             System.out.println();
         }
-        System.out.println("Matrix B");
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < matrixRow; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < matrixCol; j++)
             {
-                B[i][j] = (int)(20 * random() - 9);
+                B[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("Matrix B");
+        for(int i = 0; i < matrixRow; i++)
+        {
+            for(int j = 0; j < matrixCol; j++)
+            {
                 System.out.printf("%4d", B[i][j]);
             }
             System.out.println();
         }
         System.out.println("Matrix sumMatrix");
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < matrixRow; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < matrixCol; j++)
             {
                 //sumMatrix[i][j] = 0;
-                sumMatrix[i][j] = A[i][j] + B[j][j];
+                sumMatrix[i][j] = A[i][j] + B[i][j];
                 System.out.printf("%4d", sumMatrix[i][j]);
             }
             System.out.println();
